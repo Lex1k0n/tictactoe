e53b8a1c63c7959c8455efa04f6e7aa032e1e3d5
Lexik0n <92877722+Lex1k0n@users.noreply.github.com>
2024-05-27 10:33:26 +0300

    добавлены комментарии

--- core.py
+++ core.py
@@ -14,11 +14,13 @@ class Tile:
         self.status = 'empty'
         self.dimension = game_dimension
 
+    # отрисовка тайла на поле
     def draw(self, startpoint: list, img, size):
         self.pos_x = startpoint[0] + self.num_x * size
         self.pos_y = startpoint[1] + self.num_y * size
         self.field.blit(img, (self.pos_x, self.pos_y))
 
+    # проверка, был ли сделан клик по тайлу
     def is_collide(self, click_pos):
         if self.pos_x <= click_pos[0] <= self.pos_x + self.length and self.pos_y <= click_pos[1] <= \
                 self.pos_y + self.width:
@@ -26,6 +28,7 @@ class Tile:
         else:
             return False
 
+    # возвращение тайла к дефолт значению
     def restart_tile(self):
         self.status = 'empty'
 
@@ -43,6 +46,8 @@ class Window:
         self.main_diag = []
         self.side_diag = []
         self.game_over = False
+
+        # добавление надписей
         self.font = pg.font.Font(None, 24)
         self.status = self.font.render('Игра началась!', 1, (180, 0, 0))
         self.field.blit(self.status, (10, 50))
@@ -52,6 +57,7 @@ class Window:
             self.main_diag.append([i, i])
             self.side_diag.append([i, dimension - i - 1])
 
+    # создание списка тайлов
     def create_tiles(self):
         for tile in self.tiles:
             tile.restart_tile()
@@ -61,17 +67,20 @@ class Window:
                 self.tiles.append(Tile(self.tile_size, self.tile_size, self.field, x, y, self.dimension))
         self.fill_field(self.tiles)
 
+    # заполнение игрового поля
     def fill_field(self, tiles):
         empty_field = pg.image.load('img/empty_field.png')
         empty_field = pg.transform.scale(empty_field, (self.tile_size, self.tile_size))
         for tile in tiles:
             tile.draw(self.startpoint, empty_field, self.tile_size)
 
+    # поиск тайла по индексам
     def find_tile(self, num_x, num_y):
         for tile in self.tiles:
             if tile.num_x == num_x and tile.num_y == num_y:
                 return tile
 
+    # ход компьютера
     def make_turn(self):
         max_cross = 0
         max_zero = 0
@@ -79,6 +88,8 @@ class Window:
         best_free_lines = 0
         zero = pg.image.load('img/nolik.png')
         zero = pg.transform.scale(zero, (self.tile_size, self.tile_size))
+
+        # обработка ничьи и перезапуск игры
         if self.free_fields == 0:
             pg.draw.rect(self.field, [150, 150, 150], self.status.get_rect(topleft=(10, 50)))
             self.status = self.font.render('Ничья!', 1, (180, 0, 0))
@@ -90,6 +101,8 @@ class Window:
             for tile in self.tiles:
                 tile.restart_tile()
             return
+
+        # нахождение лучшего тайла для хода
         for tile in self.tiles:
             if best_tile is None and tile.status == 'empty':
                 best_tile = tile
@@ -126,12 +139,14 @@ class Window:
                         temp_cross, temp_zero = self.update_temp(temp_tile, temp_cross, temp_zero)
                         max_cross, max_zero, best_tile, best_free_lines = \
                             self.update_max(max_cross, max_zero, temp_cross, temp_zero, tile, best_tile, best_free_lines)
+        # отрисовка лучшего найденного тайла
         if self.free_fields > 0:
             best_tile.draw(self.startpoint, zero, self.tile_size)
             best_tile.status = 'zero'
             self.player_turn = True
             self.free_fields -= 1
-            print(self.free_fields)
+
+            # обработка ничьи после хода компьютера
             if self.free_fields == 0:
                 pg.draw.rect(self.field, [150, 150, 150], self.status.get_rect(topleft=(10, 50)))
                 self.status = self.font.render('Ничья!', 1, (180, 0, 0))
@@ -144,6 +159,8 @@ class Window:
                 self.free_fields = self.dimension ** 2
                 self.create_tiles()
                 self.player_turn = True
+
+            # обработка победы компьютера
             if self.scan_field() != 2:
                 pg.draw.rect(self.field, [150, 150, 150], self.status.get_rect(topleft=(10, 50)))
                 self.status = self.font.render('Компьютер победил!', 1, (180, 0, 0))
@@ -157,12 +174,14 @@ class Window:
                 self.create_tiles()
                 self.player_turn = True
         else:
+            # запасная проверка для исключения ошибок
             self.game_over = True
             sleep(1)
             self.game_over = False
             for tile in self.tiles:
                 tile.restart_tile()
 
+    # обновление временного максимума крестиков и ноликов по линиям
     @staticmethod
     def update_temp(tile, temp_c, temp_z):
         if tile.status == 'cross':
@@ -171,22 +190,29 @@ class Window:
             temp_z += 1
         return temp_c, temp_z
 
+    # обновление наилучшего тайла
     def update_max(self, max_c, max_z, temp_c, temp_z, tile, best_tile, best_free_lines):
         if temp_c >= max_c and best_free_lines != 999 and tile.status == 'empty':
             max_c = temp_c
+            # если выиграть следующим ходом не получается, и есть угроза проигрыша, то алгоритм будет пытаться
+            # предотвратить ее
             if max_c == self.dimension:
                 best_tile = tile
                 best_free_lines = 999
         if ((temp_z >= max_z and max_c != self.dimension) or temp_z == self.dimension) and tile.status == 'empty':
             max_z = temp_z
+            # наиболее предпочтителен тайл, который имеет большее кол-во свободных от крестиков линий
+            # если есть выбор среди нескольких
             if self.check_free_lines(tile) > best_free_lines:
                 best_tile = tile
                 best_free_lines = self.check_free_lines(tile)
+            # реализует возможность выигрыша следующим ходом
             if max_z == self.dimension:
                 best_tile = tile
                 best_free_lines = 999
         return max_c, max_z, best_tile, best_free_lines
 
+    # поиск свободных от крестиков линий для тайла
     def check_free_lines(self, tile):
         count = 0
         temp = 0
@@ -217,6 +243,8 @@ class Window:
                     count += 1
         return count
 
+    # сканирование поля на предмет выигрыша одной из сторон
+    # 0 - победа игрока, 1 - победа компьютера, 2 - продолжение игры
     def scan_field(self):
         for tile in self.tiles:
             cross_count, zero_count = 0, 0
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from core import *
 
 pg.init()
 
+# создание основного окна
 screen = pg.display.set_mode((600, 600))
 screen.fill([150, 150, 150])
 window = Window(screen, 3)
@@ -14,20 +15,29 @@ cross = pg.image.load('img/krestik.png')
 cross = pg.transform.scale(cross, (window.tile_size, window.tile_size))
 
 while True:
+
     for event in pg.event.get():
+
+        # обработчик события выхода
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
 
+        # обработчик нажатия мыши
         elif event.type == pg.MOUSEBUTTONDOWN:
             for tile in window.tiles:
+
+                # поиск нажатого тайла
                 if tile.is_collide(event.pos):
+
+                    # если тайл свободен, то отрисовывается крестик и передается ход компьютеру
                     if window.player_turn and tile.status == 'empty' and not window.game_over:
                         tile.draw(window.startpoint, cross, window.tile_size)
                         tile.status = 'cross'
                         window.free_fields -= 1
-                        print(window.free_fields)
                         window.player_turn = False
+
+                        # обработка победы игрока
                         if window.scan_field() != 2:
                             pg.draw.rect(window.field, [150, 150, 150], window.status.get_rect(topleft=(10, 50)))
                             window.status = window.font.render('Вы победили!', 1, (180, 0, 0))
@@ -39,6 +49,8 @@ while True:
                             window.field.blit(window.status, (10, 50))
                             window.create_tiles()
                             window.free_fields = window.dimension ** 2
+
+                        # обработка ничьи
                         if window.free_fields == 0:
                             pg.draw.rect(window.field, [150, 150, 150], window.status.get_rect(topleft=(10, 50)))
                             window.status = window.font.render('Ничья!', 1, (180, 0, 0))
@@ -51,14 +63,20 @@ while True:
                             window.create_tiles()
                             window.player_turn = True
                             window.free_fields = window.dimension ** 2
+
+                        # ход компьютера
                         else:
                             window.make_turn()
+
+                            # запасная обработка исключения
                             if window.scan_field() != 2:
                                 window.game_over = True
                                 sleep(1)
                                 window.game_over = False
                                 window.create_tiles()
                                 window.free_fields = window.dimension ** 2
+
+        # смена измерения
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_3:
                 window.dimension = 3
@@ -71,6 +89,8 @@ while True:
                 window.tile_size = 90
             else:
                 continue
+
+            # перезапуск игры после смены
             cross = pg.transform.scale(cross, (window.tile_size, window.tile_size))
             window.player_turn = True
             window.create_tiles()
